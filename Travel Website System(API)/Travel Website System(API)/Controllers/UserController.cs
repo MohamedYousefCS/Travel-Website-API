78481929212be4be16ce16edf8a6c9e82ae06b78
Yousef <mohamedyousefhoutar@gmail.com>
2024-05-23 03:29:12 +0300

    Add project structure

---
+++ Travel Website System(API)/Travel Website System(API)/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travel_Website_System_API_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
