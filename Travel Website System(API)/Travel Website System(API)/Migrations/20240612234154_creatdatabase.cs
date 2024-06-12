8b62df011277e8b67c928aa7fa9b0838f1654895
sherifebrahim <sherifebrahim440@gmail.com>
2024-06-13 02:50:21 +0300

    create branch sherif3

--- Travel Website System(API)/Travel Website System(API)/Migrations/20240612234154_creatdatabase.cs
+++ Travel Website System(API)/Travel Website System(API)/Migrations/20240612234154_creatdatabase.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Travel_Website_System_API_.Migrations
 {
     /// <inheritdoc />
-    public partial class CreateDatabase : Migration
+    public partial class creatdatabase : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
