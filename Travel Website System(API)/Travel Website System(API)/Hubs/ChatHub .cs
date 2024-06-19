607db887b946fef5cf66c1bb80caf346ca30004a
sherifebrahim <sherifebrahim440@gmail.com>
2024-06-19 06:28:35 +0300

    adding the logic for Add the client to a group and logic of Remove the client from all groups they were part of

--- Travel Website System(API)/Travel Website System(API)/Hubs/ChatHub .cs
+++ Travel Website System(API)/Travel Website System(API)/Hubs/ChatHub .cs
@@ -5,34 +5,58 @@
 
     public class ChatHub : Hub
     {
+
         // Method to broadcast messages to all connected clients
+
         public async Task SendMessage(string user, string message)
         {
             // send the message to all clients including the sender.
 
-            Clients.All.SendAsync("ReceiveMessage", user, message);
+            _ = Clients.All.SendAsync("ReceiveMessage", user, message);
 
             // Send the message to all clients except the sender
             await Clients.Others.SendAsync("ReceiveMessage", user, message);
         }
 
+
         public override async Task OnConnectedAsync()
         {
-            // Example: Notify all clients about the new connection
-            string user = Context.User.Identity.Name; // Get the username or identifier of the connected user
+
+            // Get the username or identifier of the connected user
+
+            string user = Context.User.Identity.Name; 
+
+            // Notify all clients about the new connection
+
             await Clients.All.SendAsync("UserConnected", user);
+
+            // Add the client to a group named after the user (optional)
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, user);
 
             await base.OnConnectedAsync();
         }
 
+
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            // Example: Notify all clients about the disconnection
-            string user = Context.User.Identity.Name; // Get the username or identifier of the disconnected user
+            // Get the username or identifier of the disconnected user
+
+            string user = Context.User.Identity.Name; 
+
+            // Notify all clients about the disconnection
+
             await Clients.All.SendAsync("UserDisconnected", user);
+
+            // Remove the client from all groups they were part of
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, user);
 
             await base.OnDisconnectedAsync(exception);
         }
+
+
+        //Notifying Users with new Receive message Notification
         public async Task NotifyUser(string user, string message)
         {
             await Clients.User(user).SendAsync("ReceiveNotification", message);
