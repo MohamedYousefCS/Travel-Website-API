d3a08581edb2ec7fb5c04d3b693bc446010902d3
MohamedHelmy12 <kasaby14@gmail.com>
2024-07-14 22:15:09 +0300

    swagger config

--- Travel Website System(API)/Travel Website System(API)/Program.cs
+++ Travel Website System(API)/Travel Website System(API)/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 using Travel_Website_System_API.Models;
 using Travel_Website_System_API_.DTO.PaymentClasses;
@@ -35,16 +36,45 @@ namespace Travel_Website_System_API_
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            //builder.Services.AddSwaggerGen(op=>
-            //op.SwaggerDoc("Version 1",new Microsoft.OpenApi.Models.OpenApiInfo()
-            //{
-            //                    Version = "v2",
-            //                     Title = "Web API Travel Website",
-            //                     Description = "This is a web api for Travel Website"
 
-            //})
-            //);
 
+            builder.Services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ASP.NET 5 Web API",
+                    Description = " ITI Projrcy"
+                });
+                // To Enable authorization using Swagger (JWT)
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6  IkpXVCJ9\"",
+                });
+
+          swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+          {
+          {
+          new OpenApiSecurityScheme
+          {
+          Reference = new OpenApiReference
+          {
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
+          }
+          },
+          new string[] {}
+          }
+          });
+                  });
+          
+  
             builder.Services.AddDbContext<ApplicationDBContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
             builder.Services.Configure<PayPalSettings>(builder.Configuration.GetSection("PayPal"));
 
